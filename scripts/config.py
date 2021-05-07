6a57fd6367c7b3839c2ed557919fb41a90ce0159
breuert <timo.breuer@th-koeln.de>
2021-05-07 13:37:48 +0200

    add start and end of round 01 and 02

--- scripts/config.py
+++ scripts/config.py
@@ -3,3 +3,7 @@ RESULT_DIR = '../results'
 NOT_PARTICIPATED = ['livivo_rank_pyterrier', 'lemuren_elk_docker', 'livivo_rank_precom']
 BASELINE_SYSTEMS = ['livivo_base', 'gesis_rec_pyserini']
 OUTCOME = True
+ROUND_01_START = '2021-03-01 00:00:00'
+ROUND_01_END = '2021-03-28 23:59:59'
+ROUND_02_START = '2021-04-12 00:00:00'
+ROUND_02_END = '2021-05-09 23:59:59'
