2fef5d5dc2a43655b164419e248b9c12a0b35c06
breuert <timo.breuer@th-koeln.de>
2021-05-17 14:55:41 +0200

    update ctr vs rank plot

--- scripts/ctr_vs_rank.py
+++ scripts/ctr_vs_rank.py
@@ -4,30 +4,9 @@ from config import *
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
-sns.set_style('darkgrid')
+# sns.set_style('darkgrid')
 
 FILENAME = 'livivo_ctr_vs_rank.pdf'
-
-
-def livivo_click_distribution(feedbacks):
-    click_distr = dict.fromkeys(['title',
-                                 'details',
-                                 'fulltext',
-                                 'order',
-                                 'bookmark',
-                                 'more_links',
-                                 'instock'])
-
-    for feedback in feedbacks:
-        livivo_clicks = [rf.get('livivo_clicks') for rf in feedback.clicks.values()]
-        for lc in livivo_clicks:
-            for element, click in lc.items():
-                if click:
-                    if click_distr.get(element) is None:
-                        click_distr[element] = 1
-                    else:
-                        click_distr[element] = click_distr[element] + 1
-    return click_distr
 
 
 def click_distribution(feedbacks):
@@ -50,9 +29,7 @@ def click_distribution(feedbacks):
 
 
 def main():
-
     mkdir(RESULT_DIR)
-
     ranking_systems = [s.id for s in systems.select().where(and_(systems.c.type != 'REC', systems.c.name != 'livivo_base')).execute().fetchall()]
     ranking_sessions = sessions.select(sessions.c.system_ranking.in_(ranking_systems)).execute().fetchall()
     ranking_sessions_ids = [r.id for r in ranking_sessions]
@@ -71,22 +48,7 @@ def main():
     plt.xlabel('Rank')
     plt.title('LIVIVO')
     plt.savefig(os.path.join(RESULT_DIR, FILENAME), bbox_inches='tight')
-
     plt.show()
-
-
-
-    pass
-
-
-    # lcd = livivo_click_distribution(ranking_feedbacks)
-    # lcd = dict(sorted(lcd.items(), key=lambda item: item[1], reverse=True))
-    #
-    # pd.DataFrame.from_dict(lcd, orient='index', columns=['Number of clicks']).plot.bar()
-    # plt.xlabel('SERP element')
-    # plt.ylabel('Number of clicks')
-    # plt.savefig(os.path.join(RESULT_DIR, FILENAME), bbox_inches='tight')
-    # plt.show()
 
 
 if __name__ == '__main__':
