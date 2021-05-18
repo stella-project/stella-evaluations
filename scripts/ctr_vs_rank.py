7722ff1f52e6662529d210beda1a50abde6fd332
breuert <timo.breuer@th-koeln.de>
2021-05-18 10:26:20 +0200

    add plot of GESIS

--- scripts/ctr_vs_rank.py
+++ scripts/ctr_vs_rank.py
@@ -4,9 +4,10 @@ from config import *
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
-# sns.set_style('darkgrid')
+sns.set_style('darkgrid')
 
-FILENAME = 'livivo_ctr_vs_rank.pdf'
+FILENAME_LIVIVO = 'livivo_ctr_vs_rank.pdf'
+FILENAME_GESIS = 'gesis_ctr_vs_rank.pdf'
 
 
 def click_distribution(feedbacks):
@@ -36,10 +37,10 @@ def main():
     ranking_feedbacks = system_feedbacks = feedbacks.select(feedbacks.c.session_id.in_(ranking_sessions_ids)).execute().fetchall()
     cd = click_distribution(ranking_feedbacks)
     df = pd.DataFrame(cd, index=['ctr'])
-    df_ctr = df.iloc[:, : 100].div(df.sum(axis=1), axis=0)  # ctr of first 100 ranks
+    df_ctr = df.iloc[:, : 50].div(df.sum(axis=1), axis=0)  # ctr of first 100 ranks
     df_ctr = df_ctr.transpose()
     df_ctr.index = df_ctr.index.values.astype(int)
-    ax = df_ctr.plot.bar(figsize=(8, 3), legend=False)
+    ax = df_ctr.plot.bar(figsize=(8, 3), legend=False, rot=0)
     for n, label in enumerate(ax.xaxis.get_ticklabels()):
         if label._text not in ['1', '10', '20', '30', '40', '50', '60', '70', '80', '90', '100']:
             label.set_visible(False)
@@ -47,7 +48,23 @@ def main():
     plt.ylabel('CTR')
     plt.xlabel('Rank')
     plt.title('LIVIVO')
-    plt.savefig(os.path.join(RESULT_DIR, FILENAME), bbox_inches='tight')
+    plt.savefig(os.path.join(RESULT_DIR, FILENAME_LIVIVO), bbox_inches='tight')
+    plt.show()
+
+    recommendation_systems = [s.id for s in systems.select().where(and_(systems.c.type != 'RANK')).execute().fetchall()]
+    recommendation_sessions = sessions.select(sessions.c.system_recommendation.in_(recommendation_systems)).execute().fetchall()
+    recommendation_sessions_ids = [r.id for r in recommendation_sessions]
+    recommendation_feedbacks = feedbacks.select(feedbacks.c.session_id.in_(recommendation_sessions_ids)).execute().fetchall()
+    cd = click_distribution(recommendation_feedbacks)
+    df = pd.DataFrame(cd, index=['ctr'])
+    df_ctr = df.iloc[:, : 6].div(df.sum(axis=1), axis=0)  # ctr of first 100 ranks
+    df_ctr = df_ctr.transpose()
+    df_ctr.index = df_ctr.index.values.astype(int)
+    ax = df_ctr.plot.bar(figsize=(8, 3), legend=False, rot=0)
+    plt.ylabel('CTR')
+    plt.xlabel('Rank')
+    plt.title('GESIS')
+    plt.savefig(os.path.join(RESULT_DIR, FILENAME_GESIS), bbox_inches='tight')
     plt.show()
 
 
