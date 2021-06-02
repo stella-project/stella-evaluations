a718ed065bf39d1e17a5a5afd24dd759a33a7a5e
breuert <timo.breuer@th-koeln.de>
2021-06-02 17:30:43 +0200

    adapt tables to updated definitions

--- scripts/reward.py
+++ scripts/reward.py
@@ -11,18 +11,6 @@ order = 10
 title = 1
 
 
-def sum_pairs(clicks: pd.Series):
-    total_clicks = []
-    for i in range(0, len(clicks)-1, 2):
-        sum = clicks[i] + clicks[i + 1]
-        total_clicks.append(sum)
-        total_clicks.append(sum)
-        # total_clicks[i+1] = clicks[i] + clicks[i+1]
-
-    return pd.Series(total_clicks, name='total_clicks', index=clicks.index)
-
-
-
 def main():
 
     df = pd.read_csv('../results/livivo_click_distribution.csv', index_col=0)
@@ -31,31 +19,23 @@ def main():
 
     df_rel = df.copy()
 
-    # df_rel['bookmark'] = df['bookmark'] * bookmark / df['clicks']
-    # df_rel['details'] = df['details'] * details / df['clicks']
-    # df_rel['fulltext'] = df['fulltext'] * fulltext / df['clicks']
-    # df_rel['instock'] = df['instock'] * instock / df['clicks']
-    # df_rel['more_links'] = df['more_links'] * more_links / df['clicks']
-    # df_rel['order'] = df['order'] * order / df['clicks']
-    # df_rel['title'] = df['title'] * title / df['clicks']
-
-    df_rel['bookmark'] = df['bookmark'] * df['bookmark'] * bookmark / df['clicks']
-    df_rel['details'] = df['details'] * df['details'] * details / df['clicks']
-    df_rel['fulltext'] = df['fulltext'] * df['fulltext'] * fulltext / df['clicks']
-    df_rel['instock'] = df['instock'] * df['instock'] * instock / df['clicks']
-    df_rel['more_links'] = df['more_links'] * df['more_links'] * more_links / df['clicks']
-    df_rel['order'] = df['order'] * df['order'] * order / df['clicks']
-    df_rel['title'] = df['title'] * df['title'] * title / df['clicks']
-    df_rel['rel_cum_reward'] = df_rel.iloc[:, :7].sum(axis=1)
+    df_rel['bookmark'] = df['bookmark'] * bookmark
+    df_rel['details'] = df['details'] * details
+    df_rel['fulltext'] = df['fulltext'] * fulltext
+    df_rel['instock'] = df['instock'] * instock
+    df_rel['more_links'] = df['more_links'] * more_links
+    df_rel['order'] = df['order'] * order
+    df_rel['title'] = df['title'] * title
+    df_rel['reward'] = df_rel.iloc[:, :7].sum(axis=1)
 
     df_splits = np.array_split(df_rel, len(df_rel) / 2)
     tmp = pd.Series()
     for split in df_splits:
-        total_clicks = sum(split['rel_cum_reward'])
-        split['reward'] = split['rel_cum_reward'] / total_clicks
-        tmp = pd.concat([tmp, split['reward']])
-    df['reward'] = tmp
-    print(df.to_latex())
+        total_reward = sum(split['reward'])
+        split['nreward'] = split['reward'] / total_reward
+        tmp = pd.concat([tmp, split['nreward']])
+    df['nreward'] = tmp
+    print(df.to_latex(float_format="%.4f"))
 
 
 if __name__ == '__main__':
